f1c616fbddd5da98ba4d6edacd1508cc3c56eeb5
ronak-d <ronakdodeja73@gamil.com>
2023-01-14 10:23:03 +0530

    solved issue for deployment

--- Figma-Login-UI/login-figma/src/Components/SignIn.jsx
+++ Figma-Login-UI/login-figma/src/Components/SignIn.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from '../Css/SignIn.module.css'
+import styles from '../Css/SignIn.module.css'   
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import { Typography } from '@mui/material';
@@ -18,13 +18,16 @@ const SignIn = () => {
                 </div>
 
                 <div className={styles.defineComany}>
-                    <Typography className={styles.defineComanyOne} variant="h3"> It’s a delight to have you onboard</Typography>
-                    <Typography className={styles.defineComanyTwo} variant="h6"> Help us know you better.</Typography>
-                    <Typography className={styles.defineComanyThree} variant="h6"> (This is how we optimize Wobot as per your business needs)</Typography>
+                    <Typography style={{margin:"20px 0px"}} className={styles.defineComanyOne} variant="p"> It’s a delight to have you onboard</Typography>
+                    <br />
+                    <Typography className={styles.defineComanyTwo} variant="p"> Help us know you better.</Typography>
+                    <br />
+                    <Typography className={styles.defineComanyThree} variant="p"> (This is how we optimize Wobot as per your business needs)</Typography>
                 </div>
 
                 <div className={styles.compFields}>
                     <TextField
+                    style={{margin:"12px 0px"}}
                     className={styles.textfield}
                     required
                     id="outlined-required"
@@ -33,6 +36,7 @@ const SignIn = () => {
                     />
 
                     <TextField
+                    style={{margin:"12px 0px"}}
                     className={styles.textfield}
                     id="outlined-select-currency"
                     select
@@ -43,22 +47,21 @@ const SignIn = () => {
                 </div>
 
                 <div className="styles.CompSize">
-                    <Typography className="styles.CompSizeHeading" variant="p">Company Size</Typography>
+                    <Typography style={{margin:"10px 0px"}} variant="p">Company Size</Typography>
 
                     <br />
 
-                    <ButtonGroup className="styles.CompButtongrp" variant="outlined" aria-label="outlined primary button group">
-                        <Button className="styles.CompButtongrpnew">1-20</Button>
-                        <Button className="styles.CompButtongrpnew">21-50</Button>
-                        <Button className="styles.CompButtongrpnew">50-200</Button>
-                        <Button className="styles.CompButtongrpnew">201-500</Button>
-                        <Button className="styles.CompButtongrpnew">500+</Button>
+                    <ButtonGroup style={{margin:"20px 0px"}} className="styles.CompButtongrp" variant="outlined" aria-label="outlined primary button group">
+                        <Button>1-20</Button>
+                        <Button>21-50</Button>
+                        <Button>50-200</Button>
+                        <Button>201-500</Button>
+                        <Button>500+</Button>
                     </ButtonGroup>
                 </div>
 
-                <Button className={styles.getStarted} variant="contained">Get Started</Button>
+                <Button style={{margin:"20px 0px"}} className={styles.getStarted} variant="contained">Get Started</Button>
 
-                
             </div>
 
         </div>
